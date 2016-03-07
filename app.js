bbe156e02b3fbd4d9f91ec83671bc1a1cd7ef079
Vitaly Bruger <vitalybruger@gmail.com>
2016-03-07 14:22:10 +0200

    comit 2

--- app.js
+++ app.js
@@ -1,119 +1,82 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var fs = require('fs');
+var db = require('./db.js');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
-    res.end('<div>Simple CRUD node.js+express application<br><br> <a href="/users">User list</a></div>');
+    res.end('<div>Simple CRUD node.js + express application<br><br> <a href="/users">User list</a></div>');
 });
 
 
-//git test
 app.get('/users', function(req, res) {  
-  fs.readFile('./data/users.json', {encoding: 'utf8'}, function (err, data) {
-    if (err) throw err;
-    var tblUsers = JSON.parse(data);
-    res.render('users',{page_title:"Users - Node.js",data:tblUsers});
+  db.read ( function(data) {
+    res.render('users',{page_title:"Users - Node.js",data:data});
   });
 });
+
 
 app.get('/users/add', function(req, res) {
   res.render('add_user',{page_title:"Add User - Node.js"});
 });
 
 
-app.post('/users/add', function (req, res) {
-  var POST = {};
-  if (req.method == 'POST') {
-    req.on('data', function(data) {
-      data = data.toString();
-      data = data.split('&');
-      for (var i = 0; i < data.length; i++) {
-        var _data = data[i].split("=");
-        POST[_data[0]] = _data[1];
-      }
-
-      fs.readFile('./data/users.json', {encoding: 'utf8'}, function (err, data) {
-        if (err) throw err;
-        var tblUsers = JSON.parse(data);
-
-        tblUsers.push({
-          firstName:POST['firstname'],
-          lastName:POST['lastname'],
-          age:POST['age']});
-
-        fs.writeFile('./data/users.json', JSON.stringify(tblUsers), function (err) {
-          if (err) return console.log(err);
-          res.redirect('/users');
-        });
-      });
+app.post('/users/add', function (req, res) {  
+  db.read ( function(data) {
+    var tblUsers = data;
+    tblUsers.push({
+      firstName:req.body.firstname,
+      lastName:req.body.lastname,
+      age:req.body.age});
+    db.write ( JSON.stringify(tblUsers),function() {
+      res.redirect('/users');
     });
-  }
+  });
 });    
       
 
 app.get('/users/delete/:id', function (req, res) {
   var id = req.params.id;
-  fs.readFile('./data/users.json', {encoding: 'utf8'}, function (err, data) {
-    if (err) throw err;
-    var tblUsers = JSON.parse(data);
-
-    tblUsers.splice(id-1, 1);            
-
-    fs.writeFile('./data/users.json', JSON.stringify(tblUsers), function (err) {
-      if (err) return console.log(err);             
+  db.read ( function(data) {
+    var tblUsers = data;
+    tblUsers.splice(id-1, 1); 
+    db.write ( JSON.stringify(tblUsers),function() {
       res.redirect('/users');
     });
-  });                        
+  });                 
 });
 
 
 app.get('/users/edit/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  fs.readFile('./data/users.json', {encoding: 'utf8'}, function (err, data) {
-    if (err) throw err;
-    var tblUsers = JSON.parse(data);
-    var editUser = tblUsers[id-1];
-    editUser.id = id;    
+  var id = req.params.id;  
+  db.read ( function(data) {
+    var editUser = data[id-1];
+    editUser.id = id; 
     res.render('edit_user',{page_title:"Users - Node.js",data:editUser});
-  });                        
+  });            
 });
 
 
-app.post('/users/edit/:id',function (req, res) {
-  var POST = {};
+app.post('/users/edit/:id',function (req, res) {  
   var id = req.params.id;
-  if (req.method == 'POST') {
-    req.on('data', function(data) {
-      data = data.toString();
-      data = data.split('&');
-      for (var i = 0; i < data.length; i++) {
-        var _data = data[i].split("=");
-        POST[_data[0]] = _data[1];
-      }
-
-      fs.readFile('./data/users.json', {encoding: 'utf8'}, function (err, data) {
-        if (err) throw err;
-        var tblUsers = JSON.parse(data);
-        tblUsers[id-1] = {
-          firstName:POST['firstname'],
-          lastName:POST['lastname'],
-          age:POST['age']};
-
-        fs.writeFile('./data/users.json', JSON.stringify(tblUsers), function (err) {
-          if (err) return console.log(err);
-          res.redirect('/users');
-        });
-      });
+  db.read ( function(data) {
+    var tblUsers = data;
+    tblUsers[id-1] = {
+      firstName:req.body.firstname,
+      lastName:req.body.lastname,
+      age:req.body.age};
+    db.write ( JSON.stringify(tblUsers),function() {
+      res.redirect('/users');
     });
-  }
+  });
 });
-
 
 
 app.listen(port, function () {
